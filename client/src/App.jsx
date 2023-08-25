bd4e473f59f76557b86d4b04e6c3c81a74e01ba1
SoltaneKarim <karimsoltane92@gmail.com>
2023-08-25 20:35:18 +0100

    authentification login done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,34 @@
 import './App.css';
+import Data from './components/Data.jsx';
+import React, {useState} from 'react';
+import Login from './components/Login.jsx';
+import Registration from './components/Registration.jsx';
 import Navbar from './components/NavBar.jsx';
 import ProductList from './components/ProductList.jsx';
-import Data from './components/Data.jsx';
 
 function App() {
-  return (
+  const [view, setView] = useState('Login')
+ 
+  const changeToRegistration = () => {
+    setView('Registration')
+  }
+
+  const changeToData = () => {
+    setView('Data')
+  }
+
+   return (
     <div className="App">
+      {view === 'Login' && <Login changeToData={changeToData} changeToRegistration={changeToRegistration} />}
+      {view === 'Registration' && <Registration changeToData={changeToData}/>}
+      {view === 'Data' && 
+      <div>
       <Navbar />
       <ProductList />
       <Data />
+    </div>}
     </div>
+    
   );
 }
 
